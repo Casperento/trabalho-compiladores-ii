91b5aad37543be542b11300c52beb143eef8a1d1
Casperento <44746868+Casperento@users.noreply.github.com>
2022-11-01 18:10:32 -0300

    added work info header

--- src/Frame/Frame.java
+++ src/Frame/Frame.java
@@ -1,3 +1,13 @@
+// Cabeçalho a ser colocado em todo arquivo fonte produzido pelo grupo que desenvolvou o trabalho
+
+// Nome(s) dos Discente(s): Rafael Alvarenga e Yuri Gandra
+// Matrícula:               0053108          e 0049466
+// Data: 09-10-2022
+
+
+// Declaro que sou (somos) o(s) único(s) autor(es) e responsável (éis) por este programa. Todas as partes do programa, exceto as que foram fornecidas
+// pelo professor ou copiadas das bibliotecas ou do livro Modern Compiler Implementation, foram desenvolvidas por mim. Declaro também que
+// sou (somos) o(s) responsável (éis) por todas as eventuais cópias deste programa e que não distribui (mos) nem facilitei (amos) a distribuição de cópias.
 package Frame;
 import java.util.List;
 

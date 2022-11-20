4920900a1289f0ed57172e1e61aacbe69add7749
Casperento <44746868+Casperento@users.noreply.github.com>
2022-11-20 15:54:23 -0300

    regalloc uncomment

--- src/RegAlloc/RegAlloc.java
+++ src/RegAlloc/RegAlloc.java
@@ -3,6 +3,7 @@ package RegAlloc;
 import Assem.AssemFlowGraph;
 import Assem.InstrList;
 import Frame.Frame;
+import Mips.MipsFrame;
 import Temp.Temp;
 import Temp.TempMap;
 
@@ -16,6 +17,6 @@ public class RegAlloc implements TempMap {
         instrs = il;
         AssemFlowGraph flowgraph = new AssemFlowGraph(instrs); // build flowgraph for liveness analysis
         Liveness liveness = new Liveness(flowgraph); // run liveness analysis (Liveness extends InterferenceGraph)
-//        Color regalloc = new Color(liveness, (MipsFrame) f, Temp.tempVecToList(f.registers())); // TODO: test and fix initial and registers params
+        Color regalloc = new Color(liveness, (MipsFrame) f, Temp.tempVecToList(f.registers())); // TODO: test and fix initial and registers params
     }
 }
